197aeaca13ed41814e82a4bf58eff651ee8d1428
JDanielGonza <42951796+JDanielGonza@users.noreply.github.com>
2025-08-02 12:06:17 -0600

    Add transport-pwa/.next/static/chunks/src_components_AdminDashboard_tsx_a540665c._.js 1754157976704

---
+++ transport-pwa/.next/static/chunks/src_components_AdminDashboard_tsx_a540665c._.js
@@ -0,0 +1,15 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([typeof document === "object" ? document.currentScript : undefined, {
+
+"[project]/src/components/AdminDashboard.tsx [app-client] (ecmascript, next/dynamic entry, async loader)": ((__turbopack_context__) => {
+
+__turbopack_context__.v((parentImport) => {
+    return Promise.all([
+  "static/chunks/src_79f49f96._.js",
+  "static/chunks/node_modules_d35b474c._.js",
+  "static/chunks/src_components_AdminDashboard_tsx_84a43f20._.js"
+].map((chunk) => __turbopack_context__.l(chunk))).then(() => {
+        return parentImport("[project]/src/components/AdminDashboard.tsx [app-client] (ecmascript, next/dynamic entry)");
+    });
+});
+}),
+}]);
